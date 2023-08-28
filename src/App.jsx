09811f6418180595c88bd5e62ac9939eb5ad0107
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-28 15:38:23 +0800

    add routing in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import Articles from "./components/Articles/Articles";
+import Admin from "./components/Admin/Admin";
 
 const App = () => {
-    return (
-        <div className='app'>
-            <Navbar />
+  return (
+    <div className="app">
+      <Navbar />
 
-            <main className='app_main'>{/* Perform Routing Here */}</main>
-        </div>
-    );
+      <main className="app_main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
